30e443f20682232db423052b9f3483f04c3d9a43
cannedsam102 <sambrocker0@gmail.com>
2026-02-20 23:08:29 -0500

    Built Player Controller

---
+++ Player/Scripts/player.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var speed : float = 400
+
+# Apply the movement
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# Store Direction
+	var direction : Vector2 = Vector2.ZERO
+	# Read Input
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+
+	position += direction * speed * delta
+	pass
